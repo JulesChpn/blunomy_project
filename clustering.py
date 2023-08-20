df2fb7fb456e2e4b31289043fcdefdb22a0ce83b
Jules Chapon <jchapon@veltys.com>
2023-08-20 22:34:14 +0200

    Clustering - Update : added comments/plotly graph

--- clustering.py
+++ clustering.py
@@ -19,7 +19,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import preprocessing
 
-### We import our dataframes
+### We import our dataframes and lists
 
 lidar_easy = preprocessing.lidar_easy
 lidar_medium = preprocessing.lidar_medium
@@ -27,20 +27,23 @@ lidar_hard = preprocessing.lidar_hard
 lidar_extrahard = preprocessing.lidar_extrahard
 
 list_df = preprocessing.list_df
+list_difficulty = preprocessing.list_difficulty
 
 
 ### Clustering
-# We use Kmeans to perform the clustering
+# We use k-means to perform the clustering
 # We know that for easy, hard and extrahard files, there are only 3 clusters
 # For medium file, there are 7 clusters
 
 var = ["x_rotated"]
 i = 0
+# We define the number of clusters
 for df in list_df:
     if i == 1:
         nb_clusters = 7
     else:
         nb_clusters = 3
+    # We fit the model of k-means
     model = KMeans(n_clusters=nb_clusters, init="k-means++", n_init=10, random_state=42)
     df["cluster"] = model.fit(df[var]).labels_
     i += 1

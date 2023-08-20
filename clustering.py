17169904f7cc7cebd2db741ef13caa2d12788294
Jules Chapon <jchapon@veltys.com>
2023-08-20 14:35:09 +0200

    Clustering - Update : Added comments

---
+++ clustering.py
@@ -0,0 +1,46 @@
+### Version : Python 3.10.12
+### Coding : UTF-8
+
+### We import packages
+
+# To manage dataframes
+import numpy as np
+import pandas as pd
+
+# To do the clustering
+from sklearn.cluster import KMeans
+
+# To realize graphs
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+### We import our python files
+
+import preprocessing
+
+### We import our dataframes
+
+lidar_easy = preprocessing.lidar_easy
+lidar_medium = preprocessing.lidar_medium
+lidar_hard = preprocessing.lidar_hard
+lidar_extrahard = preprocessing.lidar_extrahard
+
+list_df = preprocessing.list_df
+
+
+### Clustering
+# We use Kmeans to perform the clustering
+# We know that for easy, hard and extrahard files, there are only 3 clusters
+# For medium file, there are 7 clusters
+
+var = ["x_rotated"]
+i = 0
+for df in list_df:
+    if i == 1:
+        nb_clusters = 7
+    else:
+        nb_clusters = 3
+    model = KMeans(n_clusters=nb_clusters, init="k-means++", n_init=10, random_state=42)
+    df["cluster"] = model.fit(df[var]).labels_
+    i += 1
